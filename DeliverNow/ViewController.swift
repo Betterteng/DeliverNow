465b3b82b007ea23010e35c2cb4903cc422f96a9
Betterteng <betterteng@gmail.com>
2016-05-05 21:25:00 +1000

    Login & Register phase
    Try to complete the login and Register functions via Firebase.

--- DeliverNow/ViewController.swift
+++ DeliverNow/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+//import Firebase
 
 class ViewController: UIViewController {
+    
+    //let ref = Firebase(url: "https://delivernow-monash4039.firebaseio.com")
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil && CURRENT_USER.authData != nil {
+            self.logoutButton.hidden = false
+        }
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginAction(sender: UIButton) {
+//        ref.authUser(emailTextField.text!, password: passwordTextField.text!, withCompletionBlock: { error, authData in
+//            if error != nil {
+//                print(error)
+//            } else {
+//                let uid = authData.uid
+//                print("Login successfully with the uid: \(uid!)")
+//            }
+//        })
+        
+        let email = self.emailTextField.text
+        let password = self.passwordTextField.text
+        
+        if email != "" && password != "" {
+            FIREBASE_REF.authUser(email, password: password, withCompletionBlock: { (error, authData) -> Void in
+                if error == nil {
+                    NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
+                    print("Logged in succeessfully with the userID: \(authData.uid)")
+                    self.logoutButton.hidden = false
+                } else {
+                    print(error)
+                }
+            })
+        } else {
+            let alert = UIAlertController(title: "Sorry", message: "Empty input cannot be accepted...", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Got it", style: .Default, handler: nil)
+            alert.addAction(action)
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func logoutAction(sender: UIButton) {
+        CURRENT_USER.unauth()
+        NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")
+        self.logoutButton.hidden = true
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Pods/Target Support Files/Pods-DeliverNow/Pods-DeliverNow-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DeliverNow : NSObject
+@end
+@implementation PodsDummy_Pods_DeliverNow
+@end
