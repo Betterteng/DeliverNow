41a49ed1e60fe7b44245512991561f41c0b170b8
Betterteng <betterteng@gmail.com>
2016-05-06 22:56:58 +1000

    Can jump to welcome page
    Can jump to welcome page

--- DeliverNow/ViewController.swift
+++ DeliverNow/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var logoutButton: UIButton!
+    @IBOutlet var userNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil && CURRENT_USER.authData != nil {
+            // Jump to the welcome page.
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let secondViewController = storyboard.instantiateViewControllerWithIdentifier("WelcomPageVC") as UIViewController
+            self.presentViewController(secondViewController, animated: true, completion: nil)
+        }
         
         //Looks for single or multiple taps.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
@@ -25,18 +33,27 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         if NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil && CURRENT_USER.authData != nil {
+            
+            
+            
+            
             self.logoutButton.hidden = false
-            
+            // Get current user's ID
             let uid = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
-            
-            // Retrieve new posts as they are added to your database
+            // Retrieve current user's name property
             FIREBASE_REF.observeEventType(.ChildAdded, withBlock: { snapshot in
-                print((snapshot.value.objectForKey(uid)?.objectForKey("info")?.objectForKey("name"))!)
+                let userName = (snapshot.value.objectForKey(uid)?.objectForKey("info")?.objectForKey("name"))! as! String
+                // Set the text of userNameLabel
+                self.userNameLabel.text = userName
             })
+        } else {
+            self.userNameLabel.text = "No one logged in now..."
         }
     }
     
-    //Calls this function when the tap is recognized.
+    /*
+     Calls this function when the tap is recognized.
+     */
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
@@ -47,6 +64,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+     If everything is ok, record the login state of current user and show the logout button.
+     If users' input is invalid, let system popup relative alert to remind users.
+     */
     @IBAction func loginAction(sender: UIButton) {
         let email = self.emailTextField.text
         let password = self.passwordTextField.text
@@ -54,8 +75,19 @@ class ViewController: UIViewController {
         if email != "" && password != "" {
             FIREBASE_REF.authUser(email, password: password, withCompletionBlock: { (error, authData) -> Void in
                 if error == nil {
+                    //Record the login state of current user.
                     NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: "uid")
                     print("Logged in succeessfully with the userID: \(authData.uid)")
+                    
+                    // Jump to the welcome page.
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let secondViewController = storyboard.instantiateViewControllerWithIdentifier("WelcomPageVC") as UIViewController
+                    self.presentViewController(secondViewController, animated: true, completion: nil)
+                    
+                    
+                    
+                    
+                    // Hide the logout button.
                     self.logoutButton.hidden = false
                 } else {
                     print(error.description)
@@ -70,11 +102,13 @@ class ViewController: UIViewController {
     
     /*
      Loguot current user and hide the logout button in the View.
+     Change the text of userNameLabel.
      */
     @IBAction func logoutAction(sender: UIButton) {
         CURRENT_USER.unauth()
         NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")
         self.logoutButton.hidden = true
+        userNameLabel.text = "No one logged in now..."
     }
     
     /*
@@ -89,6 +123,11 @@ class ViewController: UIViewController {
             self.presentViewController(alert, animated: true, completion: nil)
         } else if error.rangeOfString("-6") != nil {
             let alert = UIAlertController(title: "Sorry", message: "The specified password is incorrect...", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Got it", style: .Default, handler: nil)
+            alert.addAction(action)
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else if error.rangeOfString("-8") != nil {
+            let alert = UIAlertController(title: "Sorry", message: "The specified user does not exist...", preferredStyle: .Alert)
             let action = UIAlertAction(title: "Got it", style: .Default, handler: nil)
             alert.addAction(action)
             self.presentViewController(alert, animated: true, completion: nil)
